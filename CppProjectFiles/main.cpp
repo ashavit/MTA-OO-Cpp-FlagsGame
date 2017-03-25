d0678b1965bffa4fdea7497926edf33b3da0ba12
Amir Shavit <amir.shavit@autodesk.com>
2017-03-25 11:49:22 +0300

    Create first main file

---
+++ CppProjectFiles/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cpp-FlagsProject
+//
+//  Created by Amir Shavit on 25/03/2017.
+//  Copyright © 2017 Amir Shavit. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# MTA-OO-Cpp-HomeProject
+# MTA-OO-Cpp-FlagsGame
+
+Flags is a home work project for cpp class @ MTA computer science 
