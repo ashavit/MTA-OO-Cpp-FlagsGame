342a168aafc5c787d56232ea30bb79b619584164
Yegor Yegorov <Gochkin@gmail.com>
2017-04-04 08:26:46 +0300

    Add board distractor

--- CppProjectFiles/Board.h
+++ CppProjectFiles/Board.h
@@ -5,20 +5,29 @@
 #include "Cell.h"
 
 class Board {
-    size_t  hight;
+    size_t  height;
     size_t  width;
     Cell*** board;
 
 public:
     Board() : Board(13, 13) {}
-    Board(size_t hight, size_t width) : hight(hight), width(width) {
-        board = new Cell**[hight];
-        for (size_t i = 0; i < hight; ++i) {
+    Board(size_t height, size_t width) : height(height), width(width) {
+        board = new Cell**[height];
+        for (size_t i = 0; i < height; ++i) {
             board[i] = new Cell*[width];
             for (size_t j = 0; j < width; ++j) {
                 board[i][j] = new Cell(i,j);
             }
         }
+    }
+    ~Board() {
+        for (size_t i = 0; i < height; ++i) {
+            for (size_t j = 0; j < width; ++j) {
+                delete board[i][j];
+            }
+            delete board[i];
+        }
+        delete board;
     }
 
     Cell& getCellatPosition(size_t row, size_t column) {
