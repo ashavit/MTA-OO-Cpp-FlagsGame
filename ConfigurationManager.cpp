81cb08a5a1b2bd65d661a8ce4645da8cfa091b3f
Amir Shavit <ashavit3@gmail.com>
2017-05-10 23:22:34 +0200

    Add out of bounds check for params

--- ConfigurationManager.cpp
+++ ConfigurationManager.cpp
@@ -26,19 +26,25 @@ void ConfigurationManager::setup(int argc, const char * argv[]) {
     for (int i = 0; i < argc; ++i) {
         std::string key = argv[i];
         if (key == PARAM_KEY_BOARD) {
-            _boardMode = ( (strcmp(argv[i+1], PARAM_VAL_FILE) == 0) ? BoardMode::BOARD_FILE : BoardMode::RANDOM);
+			// Make sure not out of bounds
+			_boardMode = ( ((i + 1 < argc) && strcmp(argv[i+1], PARAM_VAL_FILE) == 0) ? BoardMode::BOARD_FILE : BoardMode::RANDOM);
         }
         else if (key == PARAM_KEY_MOVES) {
-            _movesMode = ( (strcmp(argv[i+1], PARAM_VAL_FILE) == 0) ? MovesMode::MOVES_FILE : MovesMode::KEYBOARD);
+			// Make sure not out of bounds
+			_movesMode = ( ((i + 1 < argc) && strcmp(argv[i+1], PARAM_VAL_FILE) == 0) ? MovesMode::MOVES_FILE : MovesMode::KEYBOARD);
         }
         else if (key == PARAM_KEY_PATH) {
-            _path = argv[i+1];
+			// Make sure not out of bounds
+			if (i + 1 < argc)
+				_path = argv[i+1];
         }
         else if (key == PARAM_KEY_QUIET) {
             _quietMode = true;
         }
         else if (key == PARAM_KEY_DELAY) {
-            _delay = atoi(argv[i+1]);
+			// Make sure not out of bounds
+			if (i + 1 < argc)
+				_delay = atoi(argv[i+1]);
         }
     }
 }
@@ -58,7 +64,7 @@ std::string ConfigurationManager::path() {
 
 // This option is relevant only with -board f -moves f otherwise ignored
 bool ConfigurationManager::quietMode() {
-	// Quiet can be enabled only for all files game
+	// Quiet can be enabled only if board and moves are loaded from files
     return _quietMode && (_boardMode == BoardMode::BOARD_FILE) && (_movesMode == MovesMode::MOVES_FILE);
 }
 
