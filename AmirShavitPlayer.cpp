52a86903ea04523604199534fc4dc53568385097
Amir Shavit <ashavit3@gmail.com>
2017-06-12 10:18:06 +0200

    Add name space to algo class

--- AmirShavitPlayer.cpp
+++ AmirShavitPlayer.cpp
@@ -3,166 +3,169 @@
 #include <string>
 #include <functional>
 
-// TODO: Replace with ID
-AlgorithmRegistration algo_AmirShavitStudent("algo_AmirShavitStudent", [] { return new AmirShavitPlayer(); });
+namespace AmirShavitSpace {
 
-int AmirShavitPlayer::aliveIns = 0;
+	// TODO: Replace with ID
+	AlgorithmRegistration algo_AmirShavitStudent("algo_AmirShavitStudent", [] { return new AmirShavitPlayer(); });
 
-AmirShavitPlayer::AmirShavitPlayer() {
-	++aliveIns;
-}
+	int AmirShavitPlayer::aliveIns = 0;
+
+	AmirShavitPlayer::AmirShavitPlayer() {
+		++aliveIns;
+	}
 
 
-AmirShavitPlayer::~AmirShavitPlayer() {
-	--aliveIns;
-}
+	AmirShavitPlayer::~AmirShavitPlayer() {
+		--aliveIns;
+	}
 
-//*********** Private functions ***********//
+	//*********** Private functions ***********//
 
-void AmirShavitPlayer::resetPlayerState() {
-	myShips.clear();
-	oponentShips.clear();
-	boardData = nullptr;
-}
+	void AmirShavitPlayer::resetPlayerState() {
+		myShips.clear();
+		oponentShips.clear();
+		boardData = nullptr;
+	}
 
-char AmirShavitPlayer::selectRandomActiveShip() {
-	char activeShip = '0';
-	do {
-		int shipIndex = std::rand() % (myShips.size());
-		auto shipItr = myShips.begin();
-		for (int i = 0; i < shipIndex; ++i) { ++shipItr; }
+	char AmirShavitPlayer::selectRandomActiveShip() {
+		char activeShip = '0';
+		do {
+			int shipIndex = std::rand() % (myShips.size());
+			auto shipItr = myShips.begin();
+			for (int i = 0; i < shipIndex; ++i) { ++shipItr; }
 
-		activeShip = shipItr->first;
+			activeShip = shipItr->first;
+			GameMove shipLastMove = shipItr->second;
+
+			// Check if ship has moved - remove from map to reassign after move
+			myShips.erase(shipItr);
+			if (boardData->charAt(shipLastMove.to_x, shipLastMove.to_y) == activeShip) {
+				myShips.emplace(activeShip, GameMove(shipLastMove.to_x, shipLastMove.to_y, shipLastMove.to_x, shipLastMove.to_y));
+			}
+			else if (boardData->charAt(shipLastMove.from_x, shipLastMove.from_y) == activeShip) {
+				myShips.emplace(activeShip, GameMove(shipLastMove.from_x, shipLastMove.from_y, shipLastMove.from_x, shipLastMove.from_y));
+			}
+			else {
+				activeShip = '0';
+			}
+		} while (activeShip < '1');
+		return activeShip;
+	}
+
+	GameMove AmirShavitPlayer::selectRandomGameMove(char activeShip) {
+		auto shipItr = myShips.find(activeShip);
 		GameMove shipLastMove = shipItr->second;
+		int fromX = shipLastMove.from_x;
+		int fromY = shipLastMove.from_y;
+		int toX = shipLastMove.to_x;
+		int toY = shipLastMove.to_y;
 
-		// Check if ship has moved - remove from map to reassign after move
+		// Remove from map to reassign after move
 		myShips.erase(shipItr);
-		if (boardData->charAt(shipLastMove.to_x, shipLastMove.to_y) == activeShip) {
-			myShips.emplace(activeShip, GameMove(shipLastMove.to_x, shipLastMove.to_y, shipLastMove.to_x, shipLastMove.to_y));
-		}
-		else if (boardData->charAt(shipLastMove.from_x, shipLastMove.from_y) == activeShip) {
-			myShips.emplace(activeShip, GameMove(shipLastMove.from_x, shipLastMove.from_y, shipLastMove.from_x, shipLastMove.from_y));
+
+		// Calc distance from flag and prioritize directions
+		Direction priorities[4];
+		int deltaX = oponentFlag.x() - fromX;
+		int deltaY = oponentFlag.y() - fromY;
+		if (abs(deltaX) > abs(deltaY)) {
+			priorities[0] = (deltaX > 0 ? Direction::RIGHT : Direction::LEFT);
+			priorities[1] = (deltaY > 0 ? Direction::DOWN : Direction::UP);
+			priorities[2] = (deltaY > 0 ? Direction::UP : Direction::DOWN);
+			priorities[3] = (deltaX > 0 ? Direction::LEFT : Direction::RIGHT);
 		}
 		else {
-			activeShip = '0';
+			priorities[0] = (deltaY > 0 ? Direction::DOWN : Direction::UP);
+			priorities[1] = (deltaX > 0 ? Direction::RIGHT : Direction::LEFT);
+			priorities[2] = (deltaX > 0 ? Direction::LEFT : Direction::RIGHT);
+			priorities[3] = (deltaY > 0 ? Direction::UP : Direction::DOWN);
 		}
-	} while (activeShip < '1');
-	return activeShip;
-}
 
-GameMove AmirShavitPlayer::selectRandomGameMove(char activeShip) {
-	auto shipItr = myShips.find(activeShip);
-	GameMove shipLastMove = shipItr->second;
-	int fromX = shipLastMove.from_x;
-	int fromY = shipLastMove.from_y;
-	int toX = shipLastMove.to_x;
-	int toY = shipLastMove.to_y;
+		Direction go = Direction::STOP;
+		for (Direction d : priorities) {
+			if (isMoveAllowed(activeShip, d, fromX, fromY)) {
+				go = d;
+				break;
+			}
+		}
 
-	// Remove from map to reassign after move
-	myShips.erase(shipItr);
-
-	// Calc distance from flag and prioritize directions
-	Direction priorities[4];
-	int deltaX = oponentFlag.x() - fromX;
-	int deltaY = oponentFlag.y() - fromY;
-	if (abs(deltaX) > abs(deltaY)) {
-		priorities[0] = (deltaX > 0 ? Direction::RIGHT : Direction::LEFT);
-		priorities[1] = (deltaY > 0 ? Direction::DOWN : Direction::UP);
-		priorities[2] = (deltaY > 0 ? Direction::UP: Direction::DOWN);
-		priorities[3] = (deltaX > 0 ? Direction::LEFT : Direction::RIGHT);
-	} else {
-		priorities[0] = (deltaY > 0 ? Direction::DOWN : Direction::UP);
-		priorities[1] = (deltaX > 0 ? Direction::RIGHT : Direction::LEFT);
-		priorities[2] = (deltaX > 0 ? Direction::LEFT : Direction::RIGHT);
-		priorities[3] = (deltaY > 0 ? Direction::UP : Direction::DOWN);
+		// Try to move
+		if (go == Direction::UP) { --toY; }
+		else if (go == Direction::DOWN) { ++toY; }
+		else if (go == Direction::LEFT) { --toX; }
+		else if (go == Direction::RIGHT) { ++toX; }
+		return GameMove(fromX, fromY, toX, toY);
 	}
 
-	Direction go = Direction::STOP;
-	for (Direction d : priorities) {
-		if (isMoveAllowed(activeShip, d, fromX, fromY)) {
-			go = d;
-			break;
+	bool AmirShavitPlayer::isMoveAllowed(char active_ship, Direction direction, int from_x, int from_y) {
+		if (direction == Direction::UP) { --from_y; }
+		else if (direction == Direction::DOWN) { ++from_y; }
+		else if (direction == Direction::LEFT) { --from_x; }
+		else if (direction == Direction::RIGHT) { ++from_x; }
+		if (from_x < 1 || from_x > BoardData::cols || from_y < 1 || from_y > BoardData::rows) {
+			return false; // Out of bounds
+		}
+		char ch = boardMap[from_x][from_y];
+		if (ch == LocationType::FORREST) {
+			return (active_ship == '2' || active_ship == '7' || active_ship == '8');
+		}
+		else if (ch == LocationType::SEA) {
+			return (active_ship == '2' || active_ship == '3' || active_ship == '7');
+		}
+		else if (myFlag.x() == from_x && myFlag.y() == from_y) {
+			return false;
+		}
+		else {
+			return true;
 		}
 	}
 
-	// Try to move
-	if (go == Direction::UP) { --toY; }
-	else if (go == Direction::DOWN) { ++toY; }
-	else if (go == Direction::LEFT) { --toX; }
-	else if (go == Direction::RIGHT) { ++toX; }
-	return GameMove(fromX, fromY, toX, toY);
-}
+	//*********** Public functions ***********//
 
-bool AmirShavitPlayer::isMoveAllowed(char active_ship, Direction direction, int from_x, int from_y) {
-	if (direction == Direction::UP) { --from_y; }
-	else if (direction == Direction::DOWN) { ++from_y; }
-	else if (direction == Direction::LEFT) { --from_x; }
-	else if (direction == Direction::RIGHT) { ++from_x; }
-	if (from_x < 1 || from_x > BoardData::cols || from_y < 1 || from_y > BoardData::rows) {
-		return false; // Out of bounds
+	void AmirShavitPlayer::setPlayer(int player) {
+		if (player == 1 || player == 2)
+			playerType = player;
 	}
-	char ch = boardMap[from_x][from_y];
-	if (ch == LocationType::FORREST) {
-		return (active_ship == '2' || active_ship == '7' || active_ship == '8');
-	}
-	else if (ch == LocationType::SEA) {
-		return (active_ship == '2' || active_ship == '3' || active_ship == '7');
-	}
-	else if (myFlag.x() == from_x && myFlag.y() == from_y) {
-		return false;
-	}
-	else {
-		return true;
-	}
-}
 
-//*********** Public functions ***********//
-
-void AmirShavitPlayer::setPlayer(int player) {
-	if (player == 1 || player == 2)
-		playerType = player;
-}
-
-void AmirShavitPlayer::init(const BoardData& board) {
-	resetPlayerState();
-	boardData = &board;
-	for (unsigned int col = 1; col <= BoardData::cols; ++col) {
-		for (unsigned int row = 1; row <= BoardData::rows; ++row) {
-			boardMap[col][row] = LocationType::REGULAR;
-			char c = board.charAt(col, row);
-			if (c == LocationType::FORREST) {
-				boardMap[col][row] = LocationType::FORREST;
-			}
-			else if (c == LocationType::SEA) {
-				boardMap[col][row] = LocationType::SEA;
-			}
-			else if (c >= '1' && c <= '9') {
-				myShips.emplace(c, GameMove(col, row, col, row));
-			}
-			else if (c == '#') {
-				oponentShips.emplace(c, GameMove(col, row, col, row));
-			}
-			else if (c == 'A') {
-				if (playerType == 1) { myFlag = Location(col, row); }
-				else if (playerType == 2) { oponentFlag = Location(col, row); }
-			}
-			else if (c == 'B') {
-				if (playerType == 1) { oponentFlag = Location(col, row); }
-				else if (playerType == 2) { myFlag = Location(col, row); }
+	void AmirShavitPlayer::init(const BoardData& board) {
+		resetPlayerState();
+		boardData = &board;
+		for (unsigned int col = 1; col <= BoardData::cols; ++col) {
+			for (unsigned int row = 1; row <= BoardData::rows; ++row) {
+				boardMap[col][row] = LocationType::REGULAR;
+				char c = board.charAt(col, row);
+				if (c == LocationType::FORREST) {
+					boardMap[col][row] = LocationType::FORREST;
+				}
+				else if (c == LocationType::SEA) {
+					boardMap[col][row] = LocationType::SEA;
+				}
+				else if (c >= '1' && c <= '9') {
+					myShips.emplace(c, GameMove(col, row, col, row));
+				}
+				else if (c == '#') {
+					oponentShips.emplace(c, GameMove(col, row, col, row));
+				}
+				else if (c == 'A') {
+					if (playerType == 1) { myFlag = Location(col, row); }
+					else if (playerType == 2) { oponentFlag = Location(col, row); }
+				}
+				else if (c == 'B') {
+					if (playerType == 1) { oponentFlag = Location(col, row); }
+					else if (playerType == 2) { myFlag = Location(col, row); }
+				}
 			}
 		}
 	}
+
+	GameMove AmirShavitPlayer::play(const GameMove& opponentsMove) {
+		char activeShip = selectRandomActiveShip();
+		GameMove res = selectRandomGameMove(activeShip);
+		myShips.emplace(activeShip, res);
+		return res;
+	}
+
+	std::string AmirShavitPlayer::getName() const {
+		return "Amir Shavit's algo player";
+	}
+
 }
-
-GameMove AmirShavitPlayer::play(const GameMove& opponentsMove) {
-	char activeShip = selectRandomActiveShip();
-	GameMove res = selectRandomGameMove(activeShip);
-	myShips.emplace(activeShip, res);
-	return res;
-}
-
-std::string AmirShavitPlayer::getName() const {
-	return "Amir Shavit's algo player";
-}
-
-
--- AmirShavitPlayer.h
+++ AmirShavitPlayer.h
@@ -3,58 +3,59 @@
 #include "AbstractPlayer.h"
 #include <map>
 
-struct Location
-{
-private:
-	int _x, _y;
-
-public:
-	Location(int x = 0, int y = 0) : _x(x), _y(y) {}
-	int x() { return _x; }
-	int y() { return _y; }
-};
-
-class AmirShavitPlayer : public AbstractPlayer
-{
-	static int aliveIns;
-
-	enum Direction { STOP, UP, DOWN, LEFT, RIGHT, INVALID };
-	enum LocationType
+namespace AmirShavitSpace {
+	struct Location
 	{
-		REGULAR = ' ',
-		FORREST = 'T',
-		SEA = 'S'
+	private:
+		int _x, _y;
+
+	public:
+		Location(int x = 0, int y = 0) : _x(x), _y(y) {}
+		int x() { return _x; }
+		int y() { return _y; }
 	};
 
-	int playerType = 0;
+	class AmirShavitPlayer : public AbstractPlayer
+	{
+		static int aliveIns;
 
-	const BoardData* boardData = nullptr;
-	/* 0 indexes will be empty and ignored, to fit index to charAt function */
-	char boardMap[BoardData::cols + 1][BoardData::rows + 1];
-	std::map<char, GameMove> myShips;
-	std::map<char, GameMove> oponentShips;
-	Location myFlag;
-	Location oponentFlag;
+		enum Direction { STOP, UP, DOWN, LEFT, RIGHT, INVALID };
+		enum LocationType
+		{
+			REGULAR = ' ',
+			FORREST = 'T',
+			SEA = 'S'
+		};
 
-	void resetPlayerState();
-	char selectRandomActiveShip();
-	GameMove selectRandomGameMove(char activeShip);
-	bool isMoveAllowed(char active_ship, Direction direction, int from_x, int from_y);
+		int playerType = 0;
 
-public:
-	AmirShavitPlayer();
-	virtual ~AmirShavitPlayer();
-	AmirShavitPlayer(AmirShavitPlayer const&) = delete;
-	void operator=(AmirShavitPlayer const&) = delete;
+		const BoardData* boardData = nullptr;
+		/* 0 indexes will be empty and ignored, to fit index to charAt function */
+		char boardMap[BoardData::cols + 1][BoardData::rows + 1];
+		std::map<char, GameMove> myShips;
+		std::map<char, GameMove> oponentShips;
+		Location myFlag;
+		Location oponentFlag;
 
-	/* player: 1 for 1-2-3 player, 2 for 7-8-9 */
-	virtual void setPlayer(int player) override;
-	virtual void init(const BoardData& board) override;
+		void resetPlayerState();
+		char selectRandomActiveShip();
+		GameMove selectRandomGameMove(char activeShip);
+		bool isMoveAllowed(char active_ship, Direction direction, int from_x, int from_y);
 
-	/* Coordinates start from 1,1 */
-	virtual GameMove play(const GameMove& opponentsMove) override;
-	virtual std::string getName() const override;
+	public:
+		AmirShavitPlayer();
+		virtual ~AmirShavitPlayer();
+		AmirShavitPlayer(AmirShavitPlayer const&) = delete;
+		void operator=(AmirShavitPlayer const&) = delete;
 
-	static int aliveInstances() { return aliveIns; }
-};
+		/* player: 1 for 1-2-3 player, 2 for 7-8-9 */
+		virtual void setPlayer(int player) override;
+		virtual void init(const BoardData& board) override;
 
+		/* Coordinates start from 1,1 */
+		virtual GameMove play(const GameMove& opponentsMove) override;
+		virtual std::string getName() const override;
+
+		static int aliveInstances() { return aliveIns; }
+	};
+}
