acd268dae4f8c8bba38e33690d2e3339d544dede
Amir Shavit <ashavit3@gmail.com>
2017-06-14 12:10:20 +0200

    Use improved GameMove

--- Game.cpp
+++ Game.cpp
@@ -51,7 +51,7 @@ void Game::run() {
 		_playerA->init(*_boardDataA);
 		_playerB->init(*_boardDataB);
 
-		InnerGameMove move = InnerGameMove();
+		GameMove move = GameMove(0, 0, 0, 0);
 		while (true) {
 			++_timeStamp;
 			AbstractPlayer* activePlayer = ((_timeStamp % 2) ? _playerA : _playerB);
@@ -177,20 +177,20 @@ void Game::handleKeyboardInput() {
 	}
 }
 
-Game::InnerGameMove Game::handlePlayerTurn(AbstractPlayer* p, InnerGameMove lastOponentMove) {
+GameMove Game::handlePlayerTurn(AbstractPlayer* p, GameMove lastOponentMove) {
 	/* Coordinates start from 1,1 */
 	FilePlayer* fPlayer = dynamic_cast<FilePlayer*>(p);
 	if (fPlayer) {
 		fPlayer->setCurrentTimeStamp(_timeStamp);
 	}
-	GameMove gm = p->play(lastOponentMove.convertToGameMove());
+	GameMove gm = p->play(lastOponentMove);
 
 	// Validate ship moved only by one step
 	int deltaX = abs(gm.from_x - gm.to_x);
 	int deltaY = abs(gm.from_y - gm.to_y);
 	if ((deltaX > 0 && deltaY > 0) || // Try to move in both directions at once
 		(deltaX + deltaY > 1)) // try to move more than 1 step a time
-		return InnerGameMove();
+		return GameMove(0, 0, 0, 0);
 
 	Cell* moveFrom = _gameBoard->getCellAt(gm.from_x - 1, gm.from_y - 1);
 	Ship* s = (moveFrom ? moveFrom->getStandingShip() : nullptr);
@@ -198,7 +198,7 @@ Game::InnerGameMove Game::handlePlayerTurn(AbstractPlayer* p, InnerGameMove last
 		// Verify ship belongs to player
 		if ((p == _playerA && s->type() > ShipType::SHIP3) ||
 			(p == _playerB && s->type() < ShipType::SHIP7)) {
-			return InnerGameMove();
+			return GameMove(0, 0, 0, 0);
 		}
 		Cell* moveTo = _gameBoard->getCellAt(gm.to_x - 1, gm.to_y - 1);
 		
@@ -226,11 +226,11 @@ Game::InnerGameMove Game::handlePlayerTurn(AbstractPlayer* p, InnerGameMove last
 				drawCellIfNeeded(old);
 			}
 
-			return InnerGameMove(gm);
+			return gm;
 		}
 	}
 	// Else don't move ship
-	return InnerGameMove();
+	return GameMove(0, 0, 0, 0);
 }
 
 void Game::handleBattle(Ship* shipA, Ship* shipB, Cell* cell) {
